272b24d3790bc79ff30ddfc7f5e83c124f0ac2c5
saitama1899 <eric.lorien92@gmail.com>
2022-01-26 12:02:49 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// Para poder leer las variables de entorno del .env
+require('dotenv').config()
+
+// Conexion a DB
+require('./mongo')
+
+const cors = require('cors')
+const express = require('express')
+
+const logger = require('./middleware/logger')
+const handleErrors = require('./middleware/handleErrors')
+const notFound = require('./middleware/notFound')
+
+const app = express()
+// CORS por defecto permitirá que tu api funcione para cualquier origen
+app.use(cors())
+
+// Parsear las request que vengan en formato json
+app.use(express.json())
+
+app.use(logger)
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hola mundo!</h1>')
+})
+
+// Aqui solo llegará si no entra en ninguna de las de arriba
+app.use(notFound)
+app.use(handleErrors)
+
+const PORT = process.env.PORT
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+// Exports para los tests
+module.exports = { app, server }
